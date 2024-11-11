9736880db0543498c36eb45b0eb0f8741d0d7027
Damario Martin <martidam002@warren.k12.in.us>
2024-11-11 10:04:12 -0500

    riodini

---
+++ main.py
@@ -0,0 +1,19 @@
+#Create a function called calculator that accepts three parameters: two numbers and an operator. The operator can be +, -, *, or /. Based on the operator, perform the respective operation and return the result.
+
+#Starter Code:
+
+def calculator(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+
+# Test cases
+print(calculator(10, 5, '+'))  # Output: 15
+print(calculator(10, 5, '-'))  # Output: 5
+print(calculator(10, 5, '*'))  # Output: 50
+print(calculator(10, 5, '/'))  # Output: 2.0
